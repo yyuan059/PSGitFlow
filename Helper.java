fb5aa4c9e1eae9ce6faab454f7beccbccc83bf7c
Yubing Yuan <yyuan@prontoforms.com>
2021-10-27 10:44:37 -0400

    Add code to get single entry

---
+++ Helper.java
@@ -0,0 +1,22 @@
+package com.prontoforms.testutil;
+
+import java.util.List;
+
+public class Helper {
+
+	public static String getNewDataRecordId(List<String> oldIdList, List<String> newIdList) {
+		String diff_id = null;
+		for (String id : newIdList) {
+			if (!oldIdList.contains(id)) {
+				diff_id = id;
+				break;
+			}
+		}
+
+		return diff_id;
+	}
+
+	public static boolean isTimeOut(Long startTime, Long dueInMillis) {
+		return (startTime + dueInMillis) < System.currentTimeMillis();
+	}
+}
